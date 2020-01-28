4192b27908d92655948ecbb04d07f2a0087f85b3
Dana Zinatbakhsh <dtzinatbakhsh@gmail.com>
2020-01-27 18:55:22 -0800

    attempted palindromes got stuck

--- HashMap_drills.js
+++ HashMap_drills.js
@@ -16,9 +16,29 @@ function main() {
   lotr.set('LadyOfLight', 'Galadriel');
   lotr.set('HalfElven', 'Arwen');
   lotr.set('Ent', 'Treebeard');
-  
 
   console.log(lotr);
 }
 
 main();
+
+function palindrome(string) {
+  let pd = new HashMap(
+    string.length * 2
+  );
+  pd.MAX_LOAD_RATIO = 0.5;
+  pd.SIZE_RATIO = 3;
+  let word = string.split('');
+
+  for (let letter of word) {
+    pd.set(letter, 0);
+  }
+
+  for (let letter of word) {
+    pd.set(letter, pd.get(letter) + 1);
+  }
+
+  let odd = Boolean(word.length % 2);
+}
+
+function five() {}
